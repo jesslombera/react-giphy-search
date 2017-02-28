d58b6bfe2afc1155261f349c86a726798bd41d35
Jessica Lombera <jesstlombera@gmail.com>
2017-02-28 12:04:10 -0800

    added login and sign up logic, RequireAuth container

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import GifsReducer from './gifs.js';
 import ModalReducer from './modal.js';
+import AuthReducer from './auth.js';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+    auth: AuthReducer,
+    form: FormReducer,
     gifs: GifsReducer,
     modal: ModalReducer
 });
